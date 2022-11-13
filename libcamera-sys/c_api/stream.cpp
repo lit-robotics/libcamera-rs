8e09737ca023a2570a35b0921b306fd81935cef3
chemicstry <chemicstry@gmail.com>
2022-11-13 15:17:31 -0500

    Remove unused methods

--- libcamera-sys/c_api/stream.cpp
+++ libcamera-sys/c_api/stream.cpp
@@ -16,14 +16,6 @@ libcamera_size_range_t libcamera_stream_formats_range(const libcamera_stream_for
     return formats->range(*pixel_format);
 }
 
-void libcamera_stream_configuration_destroy(libcamera_stream_configuration_t *config);
-libcamera_pixel_format_t *libcamera_stream_configuration_pixel_format(libcamera_stream_configuration_t *config);
-libcamera_size_t *libcamera_stream_configuration_size(libcamera_stream_configuration_t *config);
-unsigned int *libcamera_stream_configuration_stride(libcamera_stream_configuration_t *config);
-unsigned int *libcamera_stream_configuration_frame_size(libcamera_stream_configuration_t *config);
-unsigned int *libcamera_stream_configuration_buffer_count(libcamera_stream_configuration_t *config);
-unsigned int *libcamera_stream_configuration_color_space(libcamera_stream_configuration_t *config);
-
 const libcamera_stream_formats_t *libcamera_stream_configuration_formats(const libcamera_stream_configuration_t *config) {
     return &config->formats();
 }
--- libcamera-sys/c_api/stream.h
+++ libcamera-sys/c_api/stream.h
@@ -54,7 +54,6 @@ libcamera_pixel_formats_t *libcamera_stream_formats_pixel_formats(const libcamer
 libcamera_sizes_t *libcamera_stream_formats_sizes(const libcamera_stream_formats_t* formats, const libcamera_pixel_format_t *pixel_format);
 libcamera_size_range_t libcamera_stream_formats_range(const libcamera_stream_formats_t* formats, const libcamera_pixel_format_t *pixel_format);
 
-void libcamera_stream_configuration_destroy(libcamera_stream_configuration_t *config);
 const libcamera_stream_formats_t *libcamera_stream_configuration_formats(const libcamera_stream_configuration_t *config);
 
 #ifdef __cplusplus
